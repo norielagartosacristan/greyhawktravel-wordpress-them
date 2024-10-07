cf3bd347688baf6d4505ea93e2e78ea9fcd8be9b
norielagartosacristan <norielagartosacristan@gmail.com>
2024-10-07 18:51:21 +0800

    Update single-tour.php

--- single-tour.php
+++ single-tour.php
@@ -8,9 +8,20 @@
                 echo '<div class="tour-image">';
                 the_post_thumbnail();
                 echo '</div>';
-                
+
                 echo '<div class="tour-content">';
                 the_title( '<h1>', '</h1>' );
+
+                // Display custom fields
+                $price = get_field('price');
+                $duration = get_field('duration');
+                if( $price ) {
+                    echo '<p class="tour-price">Price: $' . esc_html($price) . '</p>';
+                }
+                if( $duration ) {
+                    echo '<p class="tour-duration">Duration: ' . esc_html($duration) . ' days</p>';
+                }
+
                 the_content();
                 echo '<a href="#" class="book-tour">Book This Tour</a>';
                 echo '</div>';
