edefd2888c07ba742a15b84e60e8776e7bf39acc
norielagartosacristan <norielagartosacristan@gmail.com>
2024-10-07 19:50:49 +0800

    add

--- footer.php
+++ footer.php
@@ -1,5 +1,25 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All rights reserved.</p>
+
+
+
+<footer id="site-footer" class="site-footer">
+
+    <div class="footer-content">
+        <div class="footer-left">
+            <h2>Greyhawk Travel and Tours</h2>
+            <p>Explore the world with us! Offering the best travel and tour services.</p>
+        </div>
+        <div>
+
+        </div>
+        <div class="footer-right">
+
+        </div>
+    </div>
+
+    <div>
+      <p>&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?>. All rights reserved.</p>
+    </div>
+    
 </footer>
 
 <?php wp_footer(); ?>
