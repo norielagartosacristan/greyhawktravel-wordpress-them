e81e5387f37e5995e971db644c6cac9a269cb107
norielagartosacristan <norielagartosacristan@gmail.com>
2024-10-08 20:10:23 +0800

    Update single-tour.php

--- single-tour.php
+++ single-tour.php
@@ -29,6 +29,29 @@
         endif;
         ?>
     </section>
+     if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+
+        <?php if ( has_post_thumbnail() ) : ?>
+            <div class="tour-thumbnail">
+                <?php the_post_thumbnail( 'large' ); ?>
+            </div>
+        <?php endif; ?>
+
+        <div class="tour-details">
+            <p><?php echo __('Price:', 'tt-plugin') . ' ' . get_post_meta(get_the_ID(), '_tt_tour_price', true); ?></p>
+            <p><?php echo __('Duration:', 'tt-plugin') . ' ' . get_post_meta(get_the_ID(), '_tt_tour_duration', true) . ' ' . __('Days', 'tt-plugin'); ?></p>
+            <p><?php echo __('Location:', 'tt-plugin') . ' ' . get_post_meta(get_the_ID(), '_tt_tour_location', true); ?></p>
+        </div>
+
+        <?php echo do_shortcode('[tt_booking_form tour_id="' . get_the_ID() . '"]'); ?>
+
+    <?php endwhile;
+endif;
+    
 </main>
 
 <?php get_footer(); ?>
